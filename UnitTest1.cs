09ea0d8a29c2d9b55e47f1d5bb72d5ffbf53183a
petersaundersConestoga <78880664+petersaundersConestoga@users.noreply.github.com>
2021-10-08 15:49:43 -0400

    Initial Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+
+namespace GameStore.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void CreateGame_InputEmptyString_OutputEmptyString()
+        {
+            //arrange
+            string input = "";
+            string actual;
+            string expected = "";
+            Models.Game g = new Models.Game();
+
+            //act
+            g.GameStatusCode = input;
+            actual = g.GameStatusCode;
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Test]
+        public void CreateGame_InputNothing_OutputNothing()
+        {
+            //arrange
+            string actual;
+            string expected = "";
+            Models.Game g = new Models.Game();
+
+            //act
+            actual = g.GameStatusCode;
+
+            //assert
+            Assert.AreNotEqual(actual, expected);
+        }
+    }
+}
